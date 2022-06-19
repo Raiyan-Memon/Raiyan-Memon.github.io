7d9300d43833d0908f66ab9a1fb2585f45a28909
Raiyan Memon <raiyanmemon7860@gmail.com>
2022-06-19 22:35:23 +0530

    Initail commit

--- Movies API Integration in Javascript/script.js
+++ Movies API Integration in Javascript/script.js
@@ -3,7 +3,7 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 
-for(var i = 1; i<10; i++){
+for(var i = 1; i<100; i++){
 
     var APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page="+i;
 
