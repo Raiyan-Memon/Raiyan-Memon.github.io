cfd595ba30fd2017bf31b6f807bd7c80474bfd60
Raiyan Memon <raiyanmemon7860@gmail.com>
2022-06-22 00:32:23 +0530

    Image Optimise

--- Movies API Integration in Javascript/script.js
+++ Movies API Integration in Javascript/script.js
@@ -2,19 +2,14 @@ var APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-//API for image optimisation
-// const imgapilink = "http://api.resmush.it/ws.php?img=";
-
-
-
-
-
 
 for(var i = 1; i<=10; i++){
 
     var APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page="+i;
 
 const rootdata = document.querySelector('#root');
+
+
 
 async function getmovies(api) { 
 
@@ -30,48 +25,18 @@ async function getmovies(api) {
 
     data.forEach(
         (item) => {  
-            // console.log(item.poster_path);
 
-        // console.log(imgapilink + IMGPATH + item.poster_path);
-        // var processimg =imgapilink + IMGPATH + item.poster_path;
-        // console.log("path");
-        // console.log(processimg);
-
-
-
-        
-
-
+            console.log(item);
 
     const box  =   document.createElement("div");
     box.classList.add("custom");
 
     box.innerHTML = `
-
-   
-    <img src="${IMGPATH + item.poster_path}" alt="">
-
-    
+    <img src="${IMGPATH + item.poster_path}" loading="lazy" decoding="async" alt="">
     <h4>${item.title}</h4>
     `;
     rootdata.appendChild(box);
-
-
-
-
-
-    // alert('raiasdf');
-
-
-
-
-
-        // var imgdata = item['poster_path'];
-
-        // document.querySelector('#img').src = IMGPATH + item.poster_path;
-
       
-
     });
 
   }
@@ -116,7 +81,6 @@ function searchGet(data){
     data.forEach(
         (item) => {
             console.log(item);
-
      
             const box  =   document.createElement("div");
             box.classList.add("custom");
@@ -125,53 +89,14 @@ function searchGet(data){
             <img src="${IMGPATH + item.poster_path}" alt="">
             <h4>${item.title}</h4>
              `;
-            // rootdata.appendChild(box);
 
 
             rootdata.appendChild(box);
-
-
-            
-            
-
-
         }
     )
 
 
 }
 
-///https://image.tmdb.org/t/p/w1280/iPS4ju4Po7n7RIpR4yRMSw3k8oG.jpg
 
-
-const imgurl = 'https://image.tmdb.org/t/p/w1280/iPS4ju4Po7n7RIpR4yRMSw3k8oG.jpg';
-//images
-
-// const imgapilink1 = "http://api.resmush.it/ws.php?img=";
-// var path = 'C:\Users\raiya\Full Stack Development Course\Movies API Integration in Javascript\powerful fire hand punch24891_square_20220508_224132_032_76.jpg'
-
-
-// var dataapi = imgapilink1 + path;
-
-// console.log(imgapilink1 + path);
-// console.log(imgurl);
-
-
-// const url = 'qlty=30';
-// const params = new URLSearchParams(url);
-
-// myfun(imgapilink1 + imgurl);
-// async function myfun(data1){
-    
-//     // console.log(data1);
-//     var response = await fetch(data1,{
-//         method : 'post',
-//         body : params
-//     });     
-
-//     // console.log(response);   
-//     var data = await response.json();
-//     console.log(data);
-//     alert('raiasdf');
-// }
 
