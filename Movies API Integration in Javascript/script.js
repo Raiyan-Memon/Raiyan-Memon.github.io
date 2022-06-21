1c4d5548fea2ae96c209fd80308f9f106c9b847c
Raiyan Memon <raiyanmemon7860@gmail.com>
2022-06-21 23:13:27 +0530

    Image Optimised

--- Movies API Integration in Javascript/script.js
+++ Movies API Integration in Javascript/script.js
@@ -2,13 +2,17 @@ var APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
+//API for image optimisation
+const imgapilink = "http://api.resmush.it/ws.php?img=";
 
-for(var i = 1; i<100; i++){
+
+
+
+
+
+for(var i = 1; i<=10; i++){
 
     var APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page="+i;
-
-
-
 
 const rootdata = document.querySelector('#root');
 
@@ -26,51 +30,68 @@ async function getmovies(api) {
 
     data.forEach(
         (item) => {  
+            // console.log(item.poster_path);
 
-        console.log(item);
+        // console.log(imgapilink + IMGPATH + item.poster_path);
+        var processimg =imgapilink + IMGPATH + item.poster_path;
+        // console.log("path");
+        // console.log(processimg);
+
+
+
+        
+myfun(processimg);
+
+async function myfun(data1){
+    
+
+    //Quality for compression
+const url = 'qlty=10';
+//passing parameters in url
+const params = new URLSearchParams(url);
+
+    // console.log(data1);
+    var response = await fetch(data1,{
+        method : 'post',
+        body : params
+    });     
+
+    // console.log(params);   
+    var data = await response.json();
+    // console.log(data.dest);
+
+    var destimg = data.dest;
+    // console.log(destimg);
+
+
+    const box  =   document.createElement("div");
+    box.classList.add("custom");
+
+    box.innerHTML = `
+
+   
+    <img src="${destimg}" alt="">
+
+    
+    <h4>${item.title}</h4>
+    `;
+    rootdata.appendChild(box);
+
+
+
+
+
+    // alert('raiasdf');
+}
+
+
+
 
         // var imgdata = item['poster_path'];
 
         // document.querySelector('#img').src = IMGPATH + item.poster_path;
 
-        const box  =   document.createElement("div");
-        box.classList.add("custom");
-
-        box.innerHTML = `
-
-       
-        <img src="${IMGPATH + item.poster_path}" alt="">
-        <h4>${item.title}</h4>
-   
-
-        `;
-
-        // box.innerHTML = `
-        //  <div class="d-flex justify-content-center">
-        // <div class="card" style="width: 18rem;">   
-        // <img id="img" class="card-img-top" src="${IMGPATH + item.poster_path}" alt="Card image cap">  
-        // <div class="card-body"> 
-        // <h5 class="card-title">${item.title}  <span class="">${item.vote_average}</span></h5> 
-        // <p class="card-text"></p>
-        //  </div> 
-        //  </div>
-        //  `;
-        rootdata.appendChild(box);
-
-
-
-        // alert('raiyan');
-        // console.log(item);
-
-    //    console.log(IMGPATH + imgdata);
-
-        // document.querySelector('#img').src = item[IMGPATH + 'backdrop_path'];
-
-        // console.log(item[IMGPATH + 'backdrop_path']);
-
-        // const fetchdata = document.querySelector('#root').innerHTML = '  <div class="col-sm-3 d-flex justify-content-center">  <div class="card" style="width: 18rem;"> <img class="card-img-top" src="${IMGPATH + item["backdrop_path"] }" alt="Card image cap">  <div class="card-body">  <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>  </div> </div>';
-   
-
+      
 
     });
 
@@ -140,3 +161,38 @@ function searchGet(data){
 
 
 }
+
+///https://image.tmdb.org/t/p/w1280/iPS4ju4Po7n7RIpR4yRMSw3k8oG.jpg
+
+
+const imgurl = 'https://image.tmdb.org/t/p/w1280/iPS4ju4Po7n7RIpR4yRMSw3k8oG.jpg';
+//images
+
+// const imgapilink1 = "http://api.resmush.it/ws.php?img=";
+// var path = 'C:\Users\raiya\Full Stack Development Course\Movies API Integration in Javascript\powerful fire hand punch24891_square_20220508_224132_032_76.jpg'
+
+
+// var dataapi = imgapilink1 + path;
+
+// console.log(imgapilink1 + path);
+// console.log(imgurl);
+
+
+// const url = 'qlty=30';
+// const params = new URLSearchParams(url);
+
+// myfun(imgapilink1 + imgurl);
+// async function myfun(data1){
+    
+//     // console.log(data1);
+//     var response = await fetch(data1,{
+//         method : 'post',
+//         body : params
+//     });     
+
+//     // console.log(response);   
+//     var data = await response.json();
+//     console.log(data);
+//     alert('raiasdf');
+// }
+
